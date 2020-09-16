e9afc66e2fedb08a53bb11c03108b45ff54efbed
henzovz <murilohezo@gmail.com>
2020-09-15 21:52:05 -0300

    jwt autheticated for users

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import appointmentsRouter from "./appointments.routes";
+import sessionsRouter from "./sessions.routes";
 import usersRouter from "./users.routes";
 
 const routers = Router();
 
 routers.use("/appointments", appointmentsRouter);
 routers.use("/users", usersRouter);
+routers.use("/sessions", sessionsRouter);
 
 export default routers;
