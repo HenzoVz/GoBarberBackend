9133916cf42baa4def7a078b631e52130683e7f0
henzovz <murilohezo@gmail.com>
2020-09-20 11:46:15 -0300

    tratamento de exceções

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,34 @@
 import "reflect-metadata";
 
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import routers from "./router";
+import UploadConfig from "./config/upload";
+import AppError from "./errors/AppError";
 
 import "./database";
 
 const app = express();
 
 app.use(express.json());
+app.use("/files", express.static(UploadConfig.directory));
 app.use(routers);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 app.listen(3333, () => {
   console.log("🚀️ Server started on port 3333!");
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -4,13 +4,18 @@ import { getCustomRepository } from "typeorm";
 import Appointment from "../models/Appointment";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
 
+import AppError from "../errors/AppError";
+
 interface RequestDTO {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider_id, date }: RequestDTO): Promise<Appointment> {
+  public async execute({
+    provider_id,
+    date,
+  }: RequestDTO): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -20,7 +25,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error("This appointment is already booked");
+      throw new AppError("This appointment is already booked");
     }
 
     const appointment = appointmentsRepository.create({
